5321a147035304ff65a37b4aa62be7aa6554c74a
Janilson Varela <janilsonjvs@gmail.com>
2024-09-23 12:37:34 -0300

    feat: form de registro

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,34 @@
-export default function Register() {
-    return (
-        <>
-            <h1>Registrar Usuario</h1>
-        </>
-    )
+import { Form, Button, Container, Card, Alert } from "react-bootstrap";
+import api from "../../utils/api";
+import React, {useState} from "react";
+
+
+const initial = {
+    username: '',
+    password: '',
+    first_name: '',
+    endereco: '',
+    numero: '',
+    cep: '',
+    complemento: '',
+    bairro: '',
+    celular: '',
+    cidade: '',
+    estado: '',
 }
+
+const Register: React.FC = () => {
+    const [user, setUser] = useState(initial)
+
+    const hanbleFormRegisterUser = () => {
+
+    }
+
+    return (
+       <Container>
+            <h1>Register user</h1>
+       </Container>
+    );
+}
+
+export default Register;
